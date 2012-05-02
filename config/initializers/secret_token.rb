14730b1f1ae58ff8082f42fc03e6d22dcc14f250
Liev <liev.garcia@gmail.com>
2012-05-02 19:42:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleLinode::Application.config.secret_token = 'aefda5c399afe57789f6a4f2a16cc2ba7e5ebcfe1c3e81f3454e244fbead8e4b7c404f2da053efd67e9edd4790fc752b032a6d2c36a04e44c76d3b3dd224be7a'
